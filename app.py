24e0924801e870374b97e54e526e6e7991679ae4
hmimahm <73855958+hmimahm@users.noreply.github.com>
2020-11-03 08:53:52 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+"""
+This script runs the python_webapp_flask application using a development server.
+"""
+
+from os import environ
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '80'))
+    except ValueError:
+        PORT = 80
+    app.run(HOST, PORT)

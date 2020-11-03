cd864a3d566b816faf20973c2e8881d4f2829480
hmimahm <73855958+hmimahm@users.noreply.github.com>
2020-11-03 11:16:12 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-"""
-This script runs the python_webapp_flask application using a development server.
-"""
+import http.server
+import socketserver
 
-from os import environ
-from flask import Flask
-app = Flask(__name__)
+PORT = 9000
 
-if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '80'))
-    except ValueError:
-        PORT = 80
-    app.run(HOST, PORT)
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
